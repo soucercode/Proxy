078b593e4aae3fc3f27ade14f2b298ea910aa588
shop <dohongphuc742011@gmail.com>
2026-08-28 15:16:58 +0700

    Update PatchDefinitions.swift

--- 3105-main/ThreeOneOSFive/helpers/PatchDefinitions.swift
+++ 3105-main/ThreeOneOSFive/helpers/PatchDefinitions.swift
@@ -3,7 +3,8 @@ import Foundation
 struct PatchDefinition: Identifiable {
     let id: String
     let featureName: String
-    let assetName: String
+    let assetNameFFTH: String   // File cho Free Fire
+    let assetNameFFMAX: String  // File cho Free Fire Max
     let targetPath: String
 }
 
@@ -12,34 +13,34 @@ enum PatchDefinitions {
         PatchDefinition(
             id: "aim_body",
             featureName: "Proxy Aim Body",
-            assetName: "AimBody",
+            assetNameFFTH: "Aim Body FFTH",
+            assetNameFFMAX: "Aim Body FFMAX",
             targetPath: "Documents/contentcache/Compulsory/ios/gameassetbundles/cache_res.CfnFf59sr1SbsqQ6JqTKsEusjKs~3D"
         ),
         PatchDefinition(
             id: "aim_neck_v1",
             featureName: "Proxy Aim Neck V1",
-            assetName: "AimNeckV1",
+            assetNameFFTH: "Aim Neck V1 FFTH",
+            assetNameFFMAX: "Aim Neck V1 FFMAX",
             targetPath: "Documents/contentcache/Compulsory/ios/gameassetbundles/cache_res.CfnFf59sr1SbsqQ6JqTKsEusjKs~3D"
         ),
         PatchDefinition(
             id: "aim_neck_v2",
             featureName: "Proxy Aim Neck V2",
-            assetName: "AimNeckV2",
+            assetNameFFTH: "Aim Neck V2 FFTH",
+            assetNameFFMAX: "Aim Neck V2 FFMAX",
             targetPath: "Documents/contentcache/Compulsory/ios/gameassetbundles/cache_res.CfnFf59sr1SbsqQ6JqTKsEusjKs~3D"
         ),
         PatchDefinition(
             id: "magic_v4",
             featureName: "Magic V4",
-            assetName: "MagicV4",
+            assetNameFFTH: "Magic V4 FFTH",
+            assetNameFFMAX: "Magic V4 FFMAX",
             targetPath: "Documents/contentcache/Compulsory/ios/gameassetbundles/cache_res.CfnFf59sr1SbsqQ6JqTKsEusjKs~3D"
         )
     ]
     
     static func forFeatureName(_ name: String) -> PatchDefinition? {
         all.first { $0.featureName == name }
-    }
-    
-    static func forAssetName(_ assetName: String) -> PatchDefinition? {
-        all.first { $0.assetName == assetName }
     }
 }
